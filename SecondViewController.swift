71e81e6a944edc898d5cf6b9d525b9147658dd1b
hongtai zhang <qq251569880@gmail.com>
2015-07-07 21:59:38 +0800

    new
    project

---
+++ SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LetsGo
+//
+//  Created by 张宏台 on 15/7/7.
+//  Copyright (c) 2015年 张宏台. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
